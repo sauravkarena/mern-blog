03f026c0e4e29a2e45873b3d03e267fb9eba165c
BriskBrain Technologies <briskbraintechnologies@gmail.com>
2024-03-20 18:25:09 +0530

    Added pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { About } from './pages/About'
+import { SignIn } from './pages/SignIn'
+import { SignUp } from './pages/SignUp'
+import { Projects } from './pages/Projects'
+import { Dahsboard } from './pages/Dahsboard'
 
 function App() {
   return (
-    <div className='text-3xl text-blue-100'>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/dashboard' element={<Dahsboard />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/pages/Dahsboard.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Dahsboard = () => {
+  return (
+    <div>Dahsboard</div>
+  )
+}
