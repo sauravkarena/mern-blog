3549fa153c61fbdf28b0998f8451e67235259ec8
BriskBrain Technologies <briskbraintechnologies@gmail.com>
2024-03-27 17:22:15 +0530

    Updated SignUp page Ui

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,45 +1,46 @@
-import { Button, Label, TextInput } from "flowbite-react";
-import { Link } from "react-router-dom";
+import { Button, Label, TextInput } from 'flowbite-react';
+import { Link } from 'react-router-dom';
 
 export default function SignUp() {
   return (
-    <div className="min-h-screen mt-20">
-      <div className="flex flex-col max-w-3xl gap-5 p-3 mx-auto md:flex-row md:items-center">
+    <div className='min-h-screen mt-20'>
+      <div className='flex flex-col max-w-3xl gap-5 p-3 mx-auto md:flex-row md:items-center'>
         {/* left */}
-        <div className="flex-1">
-          <Link to="/" className="text-4xl font-bold dark:text-white">
-            <span className="px-2 py-1 text-white rounded-lg bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+        <div className='flex-1'>
+          <Link to='/' className='text-4xl font-bold dark:text-white'>
+            <span className='px-2 py-1 text-white rounded-lg bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>
               Sahand's
             </span>
             Blog
           </Link>
-          <p className="mt-5 text-sm">
-            This is a demo project. You can sign 5x up with your email and
-            password or with Google.
+          <p className='mt-5 text-sm'>
+            This is a demo project. You can sign up with your email and password
+            or with Google.
           </p>
         </div>
         {/* right */}
-        <div className="flex-1">
-          <form className="flex flex-col gap-4">
+
+        <div className='flex-1'>
+          <form className='flex flex-col gap-4'>
             <div>
-              <Label value="Your username" />
-              <TextInput type="text" placeholder="username" id="username" />
+              <Label value='Your username' />
+              <TextInput type='text' placeholder='Username' id='username' />
             </div>
             <div>
-              <Label value="Your email" />
-              <TextInput type="email" placeholder="email" id="email" />
+              <Label value='Your email' />
+              <TextInput type='text' placeholder='name@company.com' id='email' />
             </div>
             <div>
-              <Label value="Your password" />
-              <TextInput type="password" placeholder="password" id="password" />
+              <Label value='Your password' />
+              <TextInput type='text' placeholder='Password' id='password' />
             </div>
-            <Button gradientDuoTone='purpleToPink' >
+            <Button gradientDuoTone='purpleToPink' type='submit'>
               Sign Up
             </Button>
           </form>
-          <div className="flex gap-2 mt-1 text-sm">
-            <span>Have an Account ?</span>
-            <Link to='/sign-in' className="text-blue-500">
+          <div className="flex gap-2 mt-5 text-sm">
+            <span>Have an account?</span>
+            <Link to='/sign-in' className='text-blue-500'>
               Sign In
             </Link>
           </div>
