516984942f0e00fc4b9935d0e09d5e78781dd7b8
BriskBrain Technologies <briskbraintechnologies@gmail.com>
2024-03-27 16:38:51 +0530

    Added Error Handler

--- api/controllers/auth.controler.js
+++ api/controllers/auth.controler.js
@@ -1,26 +1,33 @@
-import User from "../models/user.model.js"
-import bcryptjs from "bcryptjs";
-export const signup = async(req, res) => {
-   const {username, email, password} = req.body;
-   if(!username || !email || !password || username === '' || email === '' , password === ''){
-    return req.status(400).json({message : "All field are required"});
-   }
+import User from '../models/user.model.js';
+import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
 
-   const hashpassword = bcryptjs.hashSync(password, 10);
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
 
-   const newUser = new User({
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === '' ||
+    email === '' ||
+    password === ''
+  ) {
+    next(errorHandler(400, 'All fields are required'));
+  }
+
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+
+  const newUser = new User({
     username,
     email,
-    password: hashpassword,
-   });
+    password: hashedPassword,
+  });
 
-   
-   try {
-      await newUser.save();
-      res.json('signup success');
-      
-   } catch (error) {
-      res.status(500).json({message: error.message});
-   }
-
-}
+  try {
+    await newUser.save();
+    res.json('Signup successful');
+  } catch (error) {
+    next(error);
+  }
+};
