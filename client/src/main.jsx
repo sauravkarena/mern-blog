c04bc57a779aca08d66dd41fde2e20614ad58831
BriskBrain Technologies <briskbraintechnologies@gmail.com>
2024-03-29 13:00:08 +0530

    Added redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,9 +6,7 @@ import { store } from "./redux/store.js";
 import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+  <Provider store={store}>    
+      <App />    
   </Provider>
 );
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,106 +1,109 @@
-import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
-import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import {
   signInStart,
   signInSuccess,
   signInFailure,
-} from "../redux/user/userSlice";
+} from '../redux/user/userSlice';
+
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const {loading, error: errorMessage} = useSelector(state => state.user);
-  const dispathch = useDispatch();
-  const naviagte = useNavigate();
+  const { loading, error: errorMessage } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!formData.email || !formData.password) {
-      return dispathch(signInFailure("Please fill all the fields...."))
+      return dispatch(signInFailure('Please fill all the fields'));
     }
     try {
-      dispathch(signInStart());      
-      const res = await fetch("/api/auth/signin", {
-        method: "POST",
-        headers: { "Content-type": "application/json" },
+      dispatch(signInStart());
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
       });
       const data = await res.json();
       if (data.success === false) {
-        dispathch(signInFailure(data.message));
-      }      
+        dispatch(signInFailure(data.message));
+      }
+
       if (res.ok) {
-        dispathch(signInSuccess(data));
-        naviagte("/");
+        dispatch(signInSuccess(data));
+        navigate('/');
       }
     } catch (error) {
-      dispathch(signInFailure(error.message));
+      dispatch(signInFailure(error.message));
     }
   };
   return (
-    <div className="min-h-screen mt-20">
-      <div className="flex flex-col max-w-3xl gap-5 p-3 mx-auto md:flex-row md:items-center">
+    <div className='min-h-screen mt-20'>
+      <div className='flex flex-col max-w-3xl gap-5 p-3 mx-auto md:flex-row md:items-center'>
         {/* left */}
-        <div className="flex-1">
-          <Link to="/" className="text-4xl font-bold dark:text-white">
-            <span className="px-2 py-1 text-white rounded-lg bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
-              Saurav's
+        <div className='flex-1'>
+          <Link to='/' className='text-4xl font-bold dark:text-white'>
+            <span className='px-2 py-1 text-white rounded-lg bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>
+              Sahand's
             </span>
             Blog
           </Link>
-          <p className="mt-5 text-sm">
+          <p className='mt-5 text-sm'>
             This is a demo project. You can sign in with your email and password
             or with Google.
           </p>
         </div>
         {/* right */}
 
-        <div className="flex-1">
-          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+        <div className='flex-1'>
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
             <div>
-              <Label value="Your email" />
+              <Label value='Your email' />
               <TextInput
-                type="text"
-                placeholder="name@company.com"
-                id="email"
+                type='email'
+                placeholder='name@company.com'
+                id='email'
                 onChange={handleChange}
               />
             </div>
             <div>
-              <Label value="Your password" />
+              <Label value='Your password' />
               <TextInput
-                type="text"
-                placeholder="********"
-                id="password"
+                type='password'
+                placeholder='**********'
+                id='password'
                 onChange={handleChange}
               />
             </div>
             <Button
-              gradientDuoTone="purpleToPink"
-              type="submit"
+              gradientDuoTone='purpleToPink'
+              type='submit'
               disabled={loading}
             >
               {loading ? (
                 <>
-                  <Spinner size="sm" />
-                  <span className="pl-3">Loading...</span>
+                  <Spinner size='sm' />
+                  <span className='pl-3'>Loading...</span>
                 </>
               ) : (
-                "Sign In"
+                'Sign In'
               )}
             </Button>
+          
           </form>
-          <div className="flex gap-2 mt-5 text-sm">
-            <span>Don't Have an account?</span>
-            <Link to="/sign-up" className="text-blue-500">
+          <div className='flex gap-2 mt-5 text-sm'>
+            <span>Dont Have an account?</span>
+            <Link to='/sign-up' className='text-blue-500'>
               Sign Up
             </Link>
           </div>
           {errorMessage && (
-            <Alert className="mt-5" color="failure">
+            <Alert className='mt-5' color='failure'>
               {errorMessage}
             </Alert>
           )}
